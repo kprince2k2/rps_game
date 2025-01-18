c085602cede1adbc85796d4f007d20e90dc72f12
kprince2k2 <kumarprincemails@gmail.com>
2025-01-18 14:50:57 +0530

    Game automated now is only one player game

--- game.java
+++ game.java
@@ -1,54 +1,48 @@
 import java.util.*;
 
 public class game {
-    static int player1(Scanner sc) {
-        int p1;
-        System.out.println("Player 1 Choose your option:");
+    static int player(Scanner sc) {
+        int p;
+        System.out.println("Player Choose your option:");
         System.out.println("1) Rock");
         System.out.println("2) Paper");
         System.out.println("3) Scissor");
-        p1 = sc.nextInt();
-        return p1;
-    }
-
-    static int player2(Scanner sc) {
-        int p2;
-        System.out.println("Player 2 Choose your option:");
-        System.out.println("1) Rock");
-        System.out.println("2) Paper");
-        System.out.println("3) Scissor");
-        p2 = sc.nextInt();
-        return p2;
+        p = sc.nextInt();
+        return p;
     }
 
     public static void main(String[] args) 
 	{
         Scanner sc = new Scanner(System.in);
-        int p1, p2;
+        int p, num;
+		Random ran= new Random();
+		num=ran.nextInt(3)+1;
         
-        p1 = player1(sc);
-        p2 = player2(sc);
+        p = player(sc);
         
-        if (p1 == p2) {
-            System.out.println("Draw");
+        if (p == num) {
+            System.out.println("You both choose same, it's a Draw");
         } 
-		else if (p1 == 1 && p2 == 2) {
-            System.out.println("Player 2 wins");
+		else if (p == 1 && num == 2) {
+            System.out.println("computer chose Paper,Computer wins");
         } 
-		else if (p1 == 1 && p2 == 3) {
-            System.out.println("Player 1 wins");
+		else if (p == 1 && num == 3) {
+            System.out.println("computer chose Scissor,Player wins");
         } 
-		else if (p1 == 2 && p2 == 1) {
-            System.out.println("Player 1 wins");
+		else if (p == 2 && num == 1) {
+            System.out.println("computer chose Rock,Player wins");
         } 
-		else if (p1 == 2 && p2 == 3) {
-            System.out.println("Player 2 wins");
+		else if (p == 2 && num == 3) {
+            System.out.println("computer chose Scissor,Computer wins");
         } 
-		else if (p1 == 3 && p2 == 1) {
-            System.out.println("Player 2 wins");
+		else if (p == 3 && num == 1) {
+            System.out.println("computer chose Rock,Computer wins");
         } 
-		else if (p1 == 3 && p2 == 2) {
-            System.out.println("Player 1 wins");
+		else if (p == 3 && num == 2) {
+            System.out.println("computer chose Paper,Player wins");
         }
+		else if(p > 3 || p < 1){
+			System.out.println("please choose a valid option");
+		}
     }
 }
